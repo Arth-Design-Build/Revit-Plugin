d55d7fb1a3aeec32823b917416f9102fc359210c
Soumya624 <soumyatarafder624@iitkgp.ac.in>
2023-02-01 12:14:06 +0530

    Added

--- MyRevitCommands/Commands/Schedule.cs
+++ MyRevitCommands/Commands/Schedule.cs
@@ -39,6 +39,7 @@ namespace MyRevitCommands
                 var schedules = new List<ViewSchedule>();
                 var form = new System.Windows.Forms.Form();
                 form.Size = new System.Drawing.Size(400, 400);
+                form.AutoScroll = true;
                 var checkBoxes = new List<CheckBox>();
                 foreach (ViewSchedule vs in col)
                 {
@@ -53,10 +54,57 @@ namespace MyRevitCommands
                     checkBoxes.Add(checkBox);
                     form.Controls.Add(checkBox);
                 }
+                var button1 = new Button
+                {
+                    Text = "Multiple",
+                    Left = 110,
+                    Top = checkBoxes.Count * 40 + 10
+                };
+                button1.Click += (sender, args) =>
+                {
+                    var selectedSchedules = checkBoxes
+                        .Where(x => x.Checked)
+                        .Select(x => schedules[checkBoxes.IndexOf(x)]);
+                    ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                    using (var excelEngine = new ExcelPackage())
+                    {
+                        foreach (ViewSchedule vs in selectedSchedules)
+                        {
+                            var fileName = vs.Name;
+                            if (vs.Name != null)
+                            {
+                                if (vs.Name.Length > 30)
+                                    xlSheetName = vs.Name.Substring(0, 30);
+                                else
+                                    xlSheetName = vs.Name;
+                                var ws = excelEngine.Workbook.Worksheets.Add(xlSheetName);
+                                vs.Export(MapPath, fileName + ".txt", opt);
+                                var data = File.ReadAllLines(MapPath + fileName + ".txt");
+                                File.WriteAllLines(MapPath + fileName + ".csv", data, Encoding.UTF8);
+                                File.Delete(MapPath + fileName + ".txt");
+                                for (int i = 0; i < data.Count(); i++)
+                                {
+                                    string[] fields = data.ElementAt(i).Split(',');
+                                    for (int j = 0; j < fields.Length; j++)
+                                    {
+                                        string field = fields[j];
+                                        if (field.StartsWith("\"") && field.EndsWith("\""))
+                                        {
+                                            field = field.Substring(1, field.Length - 2);
+                                        }
+                                        ws.Cells[i + 1, j + 1].Value = field;
+                                    }
+                                }
+                            }
+                        }
+                        //excelEngine.SaveAs(new FileInfo(MapPath + docName + ".xlsx"));
+                        MessageBox.Show("Exported Successfully!", "Success");
+                    }
+                };
                 var button = new Button
                 {
-                    Text = "Export",
-                    Left = 150,
+                    Text = "Single",
+                    Left = 190,
                     Top = checkBoxes.Count * 40 + 10
                 };
                 button.Click += (sender, args) =>
@@ -79,7 +127,7 @@ namespace MyRevitCommands
                             var ws = excelEngine.Workbook.Worksheets.Add(xlSheetName);
                             vs.Export(MapPath, fileName + ".txt", opt);
                             var data = File.ReadAllLines(MapPath + fileName + ".txt");
-                            File.WriteAllLines(MapPath + fileName + ".csv", data, Encoding.UTF8);
+                            //File.WriteAllLines(MapPath + fileName + ".csv", data, Encoding.UTF8);
                             File.Delete(MapPath + fileName + ".txt");
                             for (int i = 0; i < data.Count(); i++)
                             {
@@ -100,6 +148,7 @@ namespace MyRevitCommands
                     MessageBox.Show("Exported Successfully!", "Success");
                 }
                 };
+                form.Controls.Add(button1);
                 form.Controls.Add(button);
                 form.ShowDialog();
             }
