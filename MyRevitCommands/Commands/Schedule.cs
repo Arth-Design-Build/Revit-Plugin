1c47fe051bfdd3ff3ef7806804ab9581692141ca
Soumya624 <soumyatarafder624@iitkgp.ac.in>
2023-02-01 17:48:38 +0530

    2020

--- MyRevitCommands/Commands/Schedule.cs
+++ MyRevitCommands/Commands/Schedule.cs
@@ -21,6 +21,7 @@ namespace MyRevitCommands
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             string xlSheetName;
+            int flag = 0;
             var folderBrowser = new FolderBrowserDialog();
             folderBrowser.Description = "Select Folder to Save the Exported Schedules";
             if (folderBrowser.ShowDialog() == DialogResult.OK)
@@ -66,6 +67,15 @@ namespace MyRevitCommands
                     var selectedSchedules = checkBoxes
                         .Where(x => x.Checked)
                         .Select(x => schedules[checkBoxes.IndexOf(x)]);
+                    /*
+                    foreach (var items in checkBoxes)
+                    {
+                        if (items.Checked)
+                        {
+                            flag = 1;
+                        }
+                    }
+                    */
                     ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
                     using (var excelEngine = new ExcelPackage())
                     {
@@ -111,44 +121,63 @@ namespace MyRevitCommands
                 button.AutoSize = true;
                 button.Click += (sender, args) =>
                 {
-                var selectedSchedules = checkBoxes
-                    .Where(x => x.Checked)
-                    .Select(x => schedules[checkBoxes.IndexOf(x)]);
-                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-                using (var excelEngine = new ExcelPackage())
-                {
-                    foreach (ViewSchedule vs in selectedSchedules)
+                    var selectedSchedules = checkBoxes
+                        .Where(x => x.Checked)
+                        .Select(x => schedules[checkBoxes.IndexOf(x)]);
+                    /*
+                    foreach(var items in checkBoxes)
                     {
-                        var fileName = vs.Name;
-                        if (vs.Name != null)
+                        if (items.Checked)
                         {
-                            if (vs.Name.Length > 30)
-                                xlSheetName = vs.Name.Substring(0, 30);
-                            else
-                                xlSheetName = vs.Name;
-                            var ws = excelEngine.Workbook.Worksheets.Add(xlSheetName);
-                            vs.Export(MapPath, fileName + ".txt", opt);
-                            var data = File.ReadAllLines(MapPath + fileName + ".txt");
-                            //File.WriteAllLines(MapPath + fileName + ".csv", data, Encoding.UTF8);
-                            File.Delete(MapPath + fileName + ".txt");
-                            for (int i = 0; i < data.Count(); i++)
+                            flag = 1;
+                        }
+                    }
+                    */
+                    ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                    using (var excelEngine = new ExcelPackage())
+                    {
+                        foreach (ViewSchedule vs in selectedSchedules)
+                        {
+                            var fileName = vs.Name;
+                            if (vs.Name != null)
                             {
-                                string[] fields = data.ElementAt(i).Split(',');
-                                for (int j = 0; j < fields.Length; j++)
+                                if (vs.Name.Length > 30)
+                                    xlSheetName = vs.Name.Substring(0, 30);
+                                else
+                                    xlSheetName = vs.Name;
+                                var ws = excelEngine.Workbook.Worksheets.Add(xlSheetName);
+                                vs.Export(MapPath, fileName + ".txt", opt);
+                                var data = File.ReadAllLines(MapPath + fileName + ".txt");
+                                //File.WriteAllLines(MapPath + fileName + ".csv", data, Encoding.UTF8);
+                                File.Delete(MapPath + fileName + ".txt");
+                                for (int i = 0; i < data.Count(); i++)
                                 {
-                                    string field = fields[j];
-                                    if (field.StartsWith("\"") && field.EndsWith("\""))
+                                    string[] fields = data.ElementAt(i).Split(',');
+                                    for (int j = 0; j < fields.Length; j++)
                                     {
-                                        field = field.Substring(1, field.Length - 2);
+                                        string field = fields[j];
+                                        if (field.StartsWith("\"") && field.EndsWith("\""))
+                                        {
+                                            field = field.Substring(1, field.Length - 2);
+                                        }
+                                        ws.Cells[i + 1, j + 1].Value = field;
                                     }
-                                    ws.Cells[i + 1, j + 1].Value = field;
                                 }
                             }
                         }
+                        excelEngine.SaveAs(new FileInfo(MapPath + docName+ ".xlsx"));
+                        MessageBox.Show("Exported Successfully!", "Success");
+                        /*
+                        if(flag==0)
+                        {
+                            MessageBox.Show("Not Exported!", "Failed");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Exported Successfully!", "Success");
+                        }
+                        */
                     }
-                    excelEngine.SaveAs(new FileInfo(MapPath + docName+ ".xlsx"));
-                    MessageBox.Show("Exported Successfully!", "Success");
-                }
                 };
                 form.Controls.Add(button1);
                 form.Controls.Add(button);
