434dada60963a8ceb770899d41fb8df7fbe616bf
Soumya624 <soumyatarafder624@iitkgp.ac.in>
2023-03-16 12:27:47 +0530

    2022

--- MyRevitCommands/Commands/AddViewport.cs
+++ MyRevitCommands/Commands/AddViewport.cs
@@ -24,15 +24,120 @@ namespace MyRevitCommands
             Document doc = uiDoc.Document;
             int flag = 0;
 
-            // Get all sheets in the project
+            // Retrieve all ViewSheet elements
             FilteredElementCollector sheetCollector = new FilteredElementCollector(doc);
             ICollection<Element> sheets = sheetCollector.OfClass(typeof(ViewSheet)).ToElements();
+            ICollection<Element> sheets1 = new List<Element>();
+            var checkvar = 0;
+
+            // Create a form to display the sheet and view data
+            System.Windows.Forms.Form form = new System.Windows.Forms.Form();
+            form.Text = "Select Sheet to Place Viewport";
+            form.Size = new System.Drawing.Size(350, 300);
+            form.ControlBox = false;
+
+            // Extract sheet numbers and ids from ViewSheet elements
+            List<Tuple<string, ElementId>> sheetInfo = new List<Tuple<string, ElementId>>();
+            foreach (Element sheet in sheets)
+            {
+                Parameter parameter = sheet.get_Parameter(BuiltInParameter.SHEET_NUMBER);
+                if (parameter != null && !string.IsNullOrEmpty(parameter.AsString()))
+                {
+                    string sheetNumber = parameter.AsString();
+                    ElementId sheetId = sheet.Id;
+                    sheetInfo.Add(new Tuple<string, ElementId>(sheetNumber, sheetId));
+                }
+            }
+
+            // Create a checked list box to hold the unplaced view names
+            CheckedListBox checkedListBox1 = new CheckedListBox();
+            checkedListBox1.CheckOnClick = true;
+            checkedListBox1.Width = 310;
+            checkedListBox1.Height = 200;
+
+            // Create a Button to display the selected sheet ids
+            Button buttonX = new Button();
+            buttonX.Anchor = AnchorStyles.None;
+
+            foreach (Tuple<string, ElementId> item in sheetInfo)
+            {
+                checkedListBox1.Items.Add(item.Item1);
+            }
+
+            buttonX.Text = "Place Views";
+            buttonX.Left = 120;
+            buttonX.Top = 210;
+            buttonX.AutoSize = true;
+            buttonX.Click += (sender, e) =>
+            {
+                List<string> selectedIds = new List<string>();
+                sheets1 = new List<Element>();
+                foreach (Tuple<string, ElementId> item in sheetInfo)
+                {
+                    if (checkedListBox1.CheckedItems.Contains(item.Item1))
+                    {
+                        selectedIds.Add(item.Item2.ToString());
+                    }
+                }
+                if (selectedIds.Count > 0)
+                {
+                    foreach (Element sheet in sheets)
+                    {
+                        if (selectedIds.Contains(sheet.Id.ToString()))
+                        {
+                            sheets1.Add(sheet);
+                        }
+                    }
+                    //TaskDialog.Show("Selected Sheets", string.Join(", ", selectedIds));
+                }
+                form.Close();
+            };
+
+            checkedListBox1.Left = 10;
+            checkedListBox1.Top = 10;
+
+            form.Controls.Add(checkedListBox1);
+            form.Controls.Add(buttonX);
+
+            form.FormBorderStyle = FormBorderStyle.FixedDialog;
+            form.MaximizeBox = false;
+            form.MinimizeBox = false;
+
+            string iconUrl = "https://www.linkpicture.com/q/favicon_77.ico";
+            WebRequest request = WebRequest.Create(iconUrl);
+            using (WebResponse response = request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                stream.CopyTo(memoryStream);
+                memoryStream.Seek(0, SeekOrigin.Begin);
+
+                if (memoryStream != null)
+                {
+                    form.Icon = new Icon(memoryStream);
+                }
+                else
+                {
+                    // Handle the case where the memory stream is null
+                }
+            }
+
+            form.BackColor = System.Drawing.Color.LightGray;
+            form.ShowDialog();
+
+            //TaskDialog.Show("Compare", sheets.Count.ToString() + sheets1.Count.ToString());
+
+            // Create a form to display the sheet and view data
+            System.Windows.Forms.Form form1 = new System.Windows.Forms.Form();
+            form1.Text = "Add Viewport to Sheet";
+            form1.Size = new System.Drawing.Size(1000, 500);
+            form1.Padding = new System.Windows.Forms.Padding(10, 10, 0, 10);
 
             // Create a dictionary to hold the placed views for each sheet
             Dictionary<ViewSheet, List<Autodesk.Revit.DB.View>> placedViews = new Dictionary<ViewSheet, List<Autodesk.Revit.DB.View>>();
 
             // Loop through each sheet and get the views placed on it
-            foreach (ViewSheet sheet in sheets)
+            foreach (ViewSheet sheet in sheets1)
             {
                 ICollection<ElementId> viewIds = sheet.GetAllPlacedViews();
                 List<Autodesk.Revit.DB.View> views1 = new List<Autodesk.Revit.DB.View>();
@@ -51,12 +156,12 @@ namespace MyRevitCommands
             .WhereElementIsNotElementType()
             .Cast<Autodesk.Revit.DB.View>()
             .Where(v => (v.ViewType == ViewType.FloorPlan ||
-                         v.ViewType == ViewType.Elevation ||
-                         v.ViewType == ViewType.Section ||
-                         v.ViewType == ViewType.Rendering ||
-                         v.ViewType == ViewType.ThreeD ||
-                         v.ViewType == ViewType.DraftingView) &&
-                        v.CanBePrinted)
+                     v.ViewType == ViewType.Elevation ||
+                     v.ViewType == ViewType.Section ||
+                     v.ViewType == ViewType.Rendering ||
+                     v.ViewType == ViewType.ThreeD ||
+                     v.ViewType == ViewType.DraftingView) &&
+                     v.CanBePrinted)
             .Cast<Element>()
             .ToList();
 
@@ -72,7 +177,7 @@ namespace MyRevitCommands
 
             // Create a list to hold the sheet and view data
             List<object[]> sheetAndViewData = new List<object[]>();
-            foreach (ViewSheet sheet in sheets)
+            foreach (ViewSheet sheet in sheets1)
             {
                 // Get the sheet number and name
                 string sheetNumber = sheet.SheetNumber;
@@ -100,21 +205,21 @@ namespace MyRevitCommands
                 addViewsButton.Width = 100;
                 addViewsButton.Click += new EventHandler(delegate (object sender, EventArgs e)
                 {
-                // Get the selected views
-                List<Autodesk.Revit.DB.View> selectedViews = new List<Autodesk.Revit.DB.View>();
-                foreach (string selectedViewName in checkedListBox.CheckedItems)
-                {
-                    Autodesk.Revit.DB.View selectedView = null;
-                    foreach (Autodesk.Revit.DB.View view in views)
+                    // Get the selected views
+                    List<Autodesk.Revit.DB.View> selectedViews = new List<Autodesk.Revit.DB.View>();
+                    foreach (string selectedViewName in checkedListBox.CheckedItems)
                     {
-                        if (view.Name == selectedViewName.Split(':')[1])
+                        Autodesk.Revit.DB.View selectedView = null;
+                        foreach (Autodesk.Revit.DB.View view in views)
                         {
-                            selectedView = view;
-                            break;
+                            if (view.Name == selectedViewName.Split(':')[1])
+                            {
+                                selectedView = view;
+                                break;
+                            }
                         }
+                        selectedViews.Add(selectedView);
                     }
-                    selectedViews.Add(selectedView);
-                }
 
                     using (Transaction transaction = new Transaction(doc, "Place Views"))
                     {
@@ -152,7 +257,7 @@ namespace MyRevitCommands
                             Viewport viewport = Viewport.Create(doc, sheet.Id, selectedView.Id, point);
 
                             // increment the x position for the next viewport
-                            x += viewportWidth / 2 + (viewportSpacing*12);
+                            x += viewportWidth / 2 + (viewportSpacing * 12);
 
                             count++;
                         }
@@ -167,14 +272,6 @@ namespace MyRevitCommands
                 object[] data = new object[] { sheetNumber, sheetName, placedViewNames.ToArray(), checkedListBox, addViewsButton };
                 sheetAndViewData.Add(data);
             }
-
-            // Create a form to display the sheet and view data
-            System.Windows.Forms.Form form = new System.Windows.Forms.Form();
-            form.Text = "Add Viewport to Sheet";
-            //form.Width = 1000;
-            //form.Height = 500;
-            form.Size = new System.Drawing.Size(1000, 500);
-            form.Padding = new System.Windows.Forms.Padding(10,10,0,10);
 
             // Create a table layout panel to organize the sheet and view data
             TableLayoutPanel table = new TableLayoutPanel();
@@ -218,8 +315,7 @@ namespace MyRevitCommands
             addButtonHeader.Font = new System.Drawing.Font(addButtonHeader.Font, System.Drawing.FontStyle.Bold);
             table.Controls.Add(addButtonHeader, 4, 0);
 
-            // Add the table to the form
-            form.Controls.Add(table);
+            form1.Controls.Add(table);
 
             // Add the sheet and view data to the table
             int row = 1;
@@ -243,40 +339,30 @@ namespace MyRevitCommands
                 System.Windows.Forms.CheckedListBox comboBox = data[3] as System.Windows.Forms.CheckedListBox;
                 //comboBox.Dock = DockStyle.Fill;
                 table.Controls.Add(comboBox, 3, row);
-         
+
                 Button addButton = data[4] as Button;
                 //addButton.Dock = DockStyle.Fill;
-                table.Controls.Add(addButton, 4, row);        
+                table.Controls.Add(addButton, 4, row);
 
                 row++;
             }
 
-            if (flag == 1)
+            form1.FormBorderStyle = FormBorderStyle.FixedDialog;
+            form1.MaximizeBox = false;
+            form1.MinimizeBox = false;
+
+            string iconUrl1 = "https://www.linkpicture.com/q/favicon_77.ico";
+            WebRequest request1 = WebRequest.Create(iconUrl1);
+            using (WebResponse response1 = request1.GetResponse())
+            using (Stream stream1 = response1.GetResponseStream())
+            using (MemoryStream memoryStream1 = new MemoryStream())
             {
-                form.Close();
-            }
+                stream1.CopyTo(memoryStream1);
+                memoryStream1.Seek(0, SeekOrigin.Begin);
 
-            // Define the border style of the form to a dialog box.
-            form.FormBorderStyle = FormBorderStyle.FixedDialog;
-
-            // Set the MaximizeBox to false to remove the maximize box.
-            form.MaximizeBox = false;
-
-            // Set the MinimizeBox to false to remove the minimize box.
-            form.MinimizeBox = false;
-
-            string iconUrl = "https://www.linkpicture.com/q/favicon_77.ico";
-            WebRequest request = WebRequest.Create(iconUrl);
-            using (WebResponse response = request.GetResponse())
-            using (Stream stream = response.GetResponseStream())
-            using (MemoryStream memoryStream = new MemoryStream())
-            {
-                stream.CopyTo(memoryStream);
-                memoryStream.Seek(0, SeekOrigin.Begin);
-
-                if (memoryStream != null)
+                if (memoryStream1 != null)
                 {
-                    form.Icon = new Icon(memoryStream);
+                    form1.Icon = new Icon(memoryStream1);
                 }
                 else
                 {
@@ -284,9 +370,8 @@ namespace MyRevitCommands
                 }
             }
 
-            form.BackColor = System.Drawing.Color.LightGray;
-
-            form.ShowDialog();
+            form1.BackColor = System.Drawing.Color.LightGray;
+            form1.ShowDialog();
 
             return Result.Succeeded;
         }
