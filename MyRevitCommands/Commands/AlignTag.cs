2a44962fe011d262aeec6ac320975389f4fe067f
Soumya624 <soumyatarafder624@iitkgp.ac.in>
2023-05-03 12:21:26 +0530

    align tag command added

--- MyRevitCommands/Commands/AlignTag.cs
+++ MyRevitCommands/Commands/AlignTag.cs
@@ -66,7 +66,7 @@ namespace MyRevitCommands
             //Check the current view
             if (!activeView.CropBoxActive)
             {
-                throw new Exception("Please set a crop box to the view");
+                throw new Exception("Please set a Crop Box to the View");
             }
 
             IEnumerable<IndependentTag> tags = from elem in new FilteredElementCollector(doc, activeView.Id).OfClass(typeof(IndependentTag)).WhereElementIsNotElementType()
